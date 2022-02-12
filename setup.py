ec9548efb048c68c0e785b9225205a3c25741db9
mymy-dot <killerdark@gmail.com>
2022-02-12 01:26:05 -0800

    forgot version again.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='v4dark',
-      version='0.2',
+      version='0.3',
       description='super fancy discord bot',
       packages=['v4dark'],
       install_requires=['colorlog',
