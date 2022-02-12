cfbf3999c68f903fa4f0e39d5305f408010dd8d9
mymy-dot <killerdark@gmail.com>
2022-02-12 01:46:21 -0800

    figured out proper dot env

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='v4dark',
-      version='0.5',
+      version='0.6',
       description='super fancy discord bot',
       packages=['v4dark'],
       install_requires=['colorlog',
       'discord.py',
+      'python-dotenv'
       ],
       zip_safe=False)
