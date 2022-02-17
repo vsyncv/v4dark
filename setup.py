5c066c8d49a615fb0dd10667ca62e17f658d046f
mymy-dot <killerdark@gmail.com>
2022-02-17 01:25:31 -0800

    asd

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='v4dark',
-      version='0.8',
+      version='0.9',
       description='super fancy discord bot',
       packages=['v4dark'],
       install_requires=['colorlog',
--- v4dark/first_module_file.py
+++ v4dark/first_module_file.py
@@ -1,6 +1,8 @@
 def hello_world():
     print("hello world")
 
-async def vineet_is_awesome(message):
+async def vineet_is_awesome(message,client):
+    if message.author == client.user:
+        return
     if 'vineet' in message.content:
         await message.channel.send('Vineet is awesome!')
